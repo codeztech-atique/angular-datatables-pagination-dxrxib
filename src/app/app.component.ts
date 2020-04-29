cd54f66b3d0d24ad94850d22a917098be8e31867
codeztech-atique <atique1224@gmail.com>
2020-04-29 16:59:41 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ import { Papa } from 'ngx-papaparse';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular';
-  
-  constructor(private http: HttpClient) {
-    // this.parseCSV();
+  csvData: any[] = [];
+  headerRow: any[] = [];
+  tableData = [];
+  constructor(private http: HttpClient, private papa: Papa) {
   }
   ngOnInit() {
     let table = $('#example').DataTable({
@@ -35,22 +35,61 @@ export class AppComponent {
   }
 
   nextButtonClickEvent(): void {
-    //do next particular records like  101 - 200 rows.
-    //we are calling to api
-
     console.log('next clicked')
   }
   previousButtonClickEvent(): void {
     //do previous particular the records like  0 - 100 rows.
     //we are calling to API
   }
-  parseCSV() {
-    // this.papa.parse(data,{
-    //         complete: (result) => {
-    //             console.log('Parsed: ', result);
-    //         }
+  ConvertCSVtoJSON() {
+    console.log(JSON.stringify(this.test));
+    // let csvData = '"Hello","World!"';
+    // this.papa.parse(csvData, {
+    //   complete: (results) => {
+    //     console.log('Parsed  : ', results.data[0][1]);
+    //     // console.log(results.data.length);
+    //   }
     // });
   }
+  
+  handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    var file = files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+    let orderDetails = {};
+    reader.onload = (event: any) => {
+      var csv = event.target.result; // Content of CSV file
+      this.papa.parse(csv, {
+        skipEmptyLines: true,
+        header: true,
+        complete: (results) => {
+          console.log(results);
+          this.tableData = results.data;
+          // for (let i = 0; i < results.data.length; i++) {
+          //   console.log(results.data[i]);
+
+          //   var res = results.data[i];
+          //   // Object.keys(res)
+          //   //   .forEach(function eachKey(key) { 
+          //   //     orderDetails = {
+          //   //       key: res[key]
+          //   //     };
+          //   //     this.test.push(orderDetails);
+          //   // });
+            
+           
+           
+          // }
+          // console.log(this.test);
+          console.log('Parsed: k', results.data);
+          console.log('Order Details', this.tableData);
+        }
+      });
+    }
+  }
+
+ 
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PapaParseModule } from "ngx-papaparse";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,PapaParseModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
